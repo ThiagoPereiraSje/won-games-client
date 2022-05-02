453c3de41239a7d2cf9f7c09214a9e75ac60a2ef
Thiago Pereira <thiagopereiramail@gmail.com>
2022-05-02 13:47:09 -0300

    Create renderWithTheme test helper

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -2,11 +2,16 @@ import { render, screen } from '@testing-library/react'
 import Logo from '.'
 
 describe('<Logo />', () => {
-  it('should render the heading', () => {
+  it('should render a white label by default', () => {
+    // 1º Renderizar o componente `render`
     render(<Logo />)
 
+    // 2º Selecionar o componente a ser testado `screen` (queries)
     const element = screen.getByRole('heading', { name: /Logo/i })
 
+    // 3º Interagir com o componente `event handler`
+
+    // 4º Assert do compartamento esperado `expect`
     expect(element).toBeInTheDocument()
   })
 
