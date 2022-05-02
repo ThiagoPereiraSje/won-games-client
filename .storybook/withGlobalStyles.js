e8c05e648ba037065de1436b6f14f80fe7042e91
Thiago Pereira <thiagopereiramail@gmail.com>
2022-05-02 10:34:42 -0300

    Configure theme provider with typescript support

--- .storybook/withGlobalStyles.js
+++ .storybook/withGlobalStyles.js
@@ -1,11 +1,13 @@
+import { ThemeProvider } from 'styled-components';
 import GlobalStyles from '../src/styles/global';
+import theme from '../src/styles/theme';
 
 const withGlobalStyles = (Story) => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Story />
-    </>
+    </ThemeProvider>
   )
 }
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import { ThemeProvider } from 'styled-components'
 
 import GlobalStyles from 'styles/global'
+import theme from 'styles/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Head>
         <title>React Avançado - Boilerplate</title>
         <link rel="manifest" href="/manifest.json" />
@@ -31,6 +33,6 @@ export default function App({ Component, pageProps }: AppProps) {
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   )
 }
