4035ef6e17433d33a653761766d19192eb5c6ffb
Thiago Pereira <thiagopereiramail@gmail.com>
2022-05-03 14:51:51 -0300

    Create tests for the different button sizes

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -1,18 +1,40 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 import Button from '.'
 
 describe('<Button />', () => {
-  it('should render the heading', () => {
-    render(<Button />)
+  it('should render the medium size by default', () => {
+    renderWithTheme(<Button>By now</Button>)
 
-    const element = screen.getByRole('heading', { name: /Button/i })
+    const element = screen.getByRole('button', { name: /by now/i })
 
-    expect(element).toBeInTheDocument()
+    expect(element).toHaveStyle({
+      height: '4rem',
+      padding: '0.8rem 3.2rem',
+      'font-size': '1.4rem'
+    })
   })
 
-  it('should render correctly', () => {
-    const { container } = render(<Button />)
+  it('should render the small size', () => {
+    renderWithTheme(<Button size="small">By now</Button>)
 
-    expect(container.firstChild).toMatchSnapshot()
+    const element = screen.getByRole('button', { name: /by now/i })
+
+    expect(element).toHaveStyle({
+      height: '3rem',
+      'font-size': '1.2rem'
+    })
+  })
+
+  it('should render the large size', () => {
+    renderWithTheme(<Button size="large">By now</Button>)
+
+    const element = screen.getByRole('button', { name: /by now/i })
+
+    expect(element).toHaveStyle({
+      height: '5rem',
+      padding: '0.8rem 4.0rem',
+      'font-size': '1.6rem'
+    })
   })
 })
