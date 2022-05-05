e396dec3cc8f3c5c32221690da521978cba77702
Thiago Pereira <thiagopereiramail@gmail.com>
2022-05-05 09:36:50 -0300

    Create the initial structure for the banner

---
+++ src/components/Banner/stories.tsx
@@ -0,0 +1,18 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import Banner from '.'
+
+export default {
+  title: 'Banner',
+  component: Banner,
+  args: {
+    img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+    title: 'Defy death',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season</p>',
+    buttonLabel: 'By now',
+    buttonLink: '/games/defy-death'
+  }
+} as ComponentMeta<typeof Banner>
+
+export const Basic: ComponentStory<typeof Banner> = (args) => (
+  <Banner {...args} />
+)
---
+++ src/components/Banner/test.tsx
@@ -0,0 +1,31 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+import Banner from '.'
+
+describe('<Banner />', () => {
+  it('should render correctly', () => {
+    renderWithTheme(
+      <Banner
+        img="/image"
+        title="My title"
+        subtitle="My subtitle"
+        buttonLabel="My button"
+        buttonLink="/link"
+      />
+    )
+
+    const img = screen.getByRole('img')
+    const title = screen.getByRole('heading', { name: /my title/i })
+    const subtitle = screen.getByRole('heading', { name: /my subtitle/i })
+
+    // verificar se imagem tem o atributo src
+    expect(img).toBeInTheDocument()
+    expect(img).toHaveAttribute('src', '/image')
+
+    // verificar se o título foi renderizado
+    expect(title).toBeInTheDocument()
+
+    // verificar se o subtitulo foi renderizado
+    expect(subtitle).toBeInTheDocument()
+  })
+})
