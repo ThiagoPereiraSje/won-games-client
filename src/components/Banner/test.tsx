2f5ee2d7305c10b942a44bdba88ce78a871f2cab
Thiago Pereira <thiagopereiramail@gmail.com>
2022-05-05 09:55:38 -0300

    Create the initial tests for the banner

--- src/components/Banner/test.tsx
+++ src/components/Banner/test.tsx
@@ -17,6 +17,7 @@ describe('<Banner />', () => {
     const img = screen.getByRole('img')
     const title = screen.getByRole('heading', { name: /my title/i })
     const subtitle = screen.getByRole('heading', { name: /my subtitle/i })
+    const buttonLink = screen.getByRole('link', { name: /my button/i })
 
     // verificar se imagem tem o atributo src
     expect(img).toBeInTheDocument()
@@ -27,5 +28,9 @@ describe('<Banner />', () => {
 
     // verificar se o subtitulo foi renderizado
     expect(subtitle).toBeInTheDocument()
+
+    // verificar se o link foi renderizado e tem href='link'
+    expect(buttonLink).toBeInTheDocument()
+    expect(buttonLink).toHaveAttribute('href', '/link')
   })
 })
