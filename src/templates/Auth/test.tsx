68c234726df47974a58cdeeb43d4c82af23f5014
Thiago Pereira <thiagopereiramail@gmail.com>
2022-05-12 16:15:14 -0300

    Create tests for the Auth template

--- src/templates/Auth/test.tsx
+++ src/templates/Auth/test.tsx
@@ -1,12 +1,42 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 import Auth from '.'
 
 describe('<Auth />', () => {
-  it('should render correctly', () => {
-    render(<Auth />)
+  it('should render all components and children', () => {
+    renderWithTheme(
+      <Auth title="Sign in">
+        <input type="text" />
+      </Auth>
+    )
 
-    const element = screen.getByRole('heading', { name: /Auth/i })
+    // logos
+    expect(screen.getAllByLabelText(/won games/i)).toHaveLength(2)
 
-    expect(element).toBeInTheDocument()
+    // title
+    expect(
+      screen.getByRole('heading', {
+        name: /all your favorite games in one place/i
+      })
+    ).toBeInTheDocument()
+
+    // subtitle
+    expect(
+      screen.getByRole('heading', {
+        name: /won is the best and mos complete gaming platform/i
+      })
+    ).toBeInTheDocument()
+
+    // Footer
+    expect(
+      screen.getByText(/Won Games 2020 © Todos os Direitos Reservados/i)
+    ).toBeInTheDocument()
+
+    // heading do content
+    expect(
+      screen.getByRole('heading', { name: /sign in/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('textbox')).toBeInTheDocument()
   })
 })
