3146b9403ee65f5559d40738ff3bff0d7dece79a
Thiago Pereira <thiagopereiramail@gmail.com>
2022-05-12 08:57:17 -0300

    Create tests to TextField component

--- src/components/TextField/test.tsx
+++ src/components/TextField/test.tsx
@@ -1,8 +1,64 @@
+import { screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { renderWithTheme } from 'utils/tests/helpers'
 import TextField from '.'
 
 describe('<TextField />', () => {
-  it('should render correctly', () => {
-    renderWithTheme(<TextField />)
+  it('should render with label', () => {
+    renderWithTheme(<TextField label="Label" labelFor="Field" id="Field" />)
+
+    expect(screen.getByLabelText(/label/i)).toBeInTheDocument()
+  })
+
+  it('should render without label', () => {
+    const { container } = renderWithTheme(<TextField />)
+
+    expect(container.getElementsByTagName('label')).toHaveLength(0)
+  })
+
+  it('should be accessible by tab', () => {
+    renderWithTheme(
+      <TextField label="TextField" labelFor="TextField" id="TextField" />
+    )
+
+    const input = screen.getByLabelText(/textfield/i)
+
+    expect(document.body).toHaveFocus()
+
+    userEvent.tab()
+
+    expect(input).toHaveFocus()
+  })
+
+  it('should render with placeholder', () => {
+    renderWithTheme(<TextField placeholder="Hey You" />)
+
+    expect(screen.getByPlaceholderText(/hey you/i)).toBeInTheDocument()
+  })
+
+  it('should change your value when user typing', async () => {
+    const onInput = jest.fn()
+
+    renderWithTheme(
+      <TextField
+        onInput={onInput}
+        label="TextField"
+        labelFor="TextField"
+        id="TextField"
+      />
+    )
+
+    const input = screen.getByRole('textbox')
+    const text = 'This is my new text'
+
+    userEvent.type(input, text)
+
+    // waitFor is needed because of useState
+    await waitFor(() => {
+      expect(input).toHaveValue(text)
+      expect(onInput).toHaveBeenCalledTimes(text.length)
+    })
+
+    expect(onInput).toHaveBeenCalledWith(text)
   })
 })
