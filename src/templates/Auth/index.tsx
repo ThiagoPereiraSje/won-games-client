e18e6d1c438d7f9b6de1504130a8309a8777c087
Thiago Pereira <thiagopereiramail@gmail.com>
2022-05-13 07:57:31 -0300

    Fix svg logo bug with duplicate id

--- src/templates/Auth/index.tsx
+++ src/templates/Auth/index.tsx
@@ -13,7 +13,7 @@ const Auth = ({ title, children }: AuthProps) => {
     <S.Wrapper>
       <S.BannerBlock>
         <S.BannerContent>
-          <Logo />
+          <Logo id="banner" />
 
           <div>
             <Heading>All your favorite games in one place</Heading>
@@ -27,7 +27,7 @@ const Auth = ({ title, children }: AuthProps) => {
       </S.BannerBlock>
       <S.Content>
         <S.ContentWrapper>
-          <Logo color="black" size="large" />
+          <Logo id="content" color="black" size="large" />
           <Heading color="black" lineColor="secondary" lineLeft>
             {title}
           </Heading>
