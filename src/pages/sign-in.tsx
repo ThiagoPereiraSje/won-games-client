6ff2925b89d241c1437ddd20a2131c7fb1eca223
Thiago Pereira <thiagopereiramail@gmail.com>
2022-05-14 10:25:28 -0300

    Create an accessible form

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,10 +1,20 @@
-import FormSignIn from 'components/FormSignIn'
+import Checkbox from 'components/Checkbox'
+import CheckboxSvg from 'components/CheckboxSvg'
+// import FormSignIn from 'components/FormSignIn'
 import Auth from 'templates/Auth'
 
 export default function SignIn() {
   return (
     <Auth title="Sign in">
-      <FormSignIn />
+      {/* <FormSignIn /> */}
+
+      <CheckboxSvg label="Sara Checkbox" labelFor="sara-checkbox" />
+
+      <Checkbox
+        label="Willian Checkbox"
+        labelFor="willian-checkbox"
+        labelColor="black"
+      />
     </Auth>
   )
 }
