639f9416f48ef74502ec1164c59446b82dc901ba
Thiago Pereira <thiagopereiramail@gmail.com>
2022-05-19 08:46:03 -0300

    Create the Wishlist Page template

---
+++ src/templates/Wishlist/index.tsx
@@ -0,0 +1,18 @@
+import { Container } from 'components/Container'
+import Heading from 'components/Heading'
+import Base from 'templates/Base'
+// import * as S from './styles'
+
+const Wishlist = () => {
+  return (
+    <Base>
+      <Container>
+        <Heading lineColor="secondary" lineLeft>
+          Wishlist
+        </Heading>
+      </Container>
+    </Base>
+  )
+}
+
+export default Wishlist
