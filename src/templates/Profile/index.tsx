cd593ba33a98fabd940f1eed1ce5ae02c9b0a1a3
Thiago Pereira <thiagopereiramail@gmail.com>
2022-05-24 07:47:04 -0300

    Create profile/me page

---
+++ src/templates/Profile/index.tsx
@@ -0,0 +1,28 @@
+import Base from 'templates/Base'
+import Heading from 'components/Heading'
+import ProfileMenu from 'components/ProfileMenu'
+import { Container } from 'components/Container'
+import * as S from './styles'
+
+export type ProfileTemplateProps = {
+  children: React.ReactNode
+}
+
+const Profile = ({ children }: ProfileTemplateProps) => {
+  return (
+    <Base>
+      <Container>
+        <Heading lineLeft lineColor="secondary">
+          My Profile
+        </Heading>
+
+        <S.Main>
+          <ProfileMenu />
+          <S.Content>{children}</S.Content>
+        </S.Main>
+      </Container>
+    </Base>
+  )
+}
+
+export default Profile
