fc6073979655ef52217abe86d65273ce03918014
Thiago Pereira <thiagopereiramail@gmail.com>
2022-05-25 18:56:12 -0300

    Create the filter method and styles

--- src/components/ExploreSidebar/index.tsx
+++ src/components/ExploreSidebar/index.tsx
@@ -1,9 +1,10 @@
+import { useState } from 'react'
+import { Close, FilterList } from '@styled-icons/material-outlined'
 import Heading from 'components/Heading'
 import Checkbox from 'components/Checkbox'
 import Radio from 'components/Radio'
 import Button from 'components/Button'
 import * as S from './styles'
-import { useState } from 'react'
 
 type Field = {
   label: string
@@ -24,44 +25,80 @@ export type ItemProps = {
 export type ExploreSidebarProps = {
   items: ItemProps[]
   initialValues?: Values
+  onFilter: (values: Values) => void
 }
 
-const ExploreSidebar = ({ items, initialValues = {} }: ExploreSidebarProps) => {
-  const [values] = useState(initialValues)
+const ExploreSidebar = ({
+  items,
+  onFilter,
+  initialValues = {}
+}: ExploreSidebarProps) => {
+  const [values, setValues] = useState(initialValues)
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleFilter = () => {
+    onFilter(values)
+  }
+
+  const handleChange = (name: string, value: string | boolean) => {
+    setValues((s) => ({ ...s, [name]: value }))
+  }
 
   return (
-    <S.Wrapper>
-      {items.map((item) => (
-        <div key={item.title}>
-          <Heading lineBottom lineColor="secondary" size="small">
-            {item.title}
-          </Heading>
+    <S.Wrapper isOpen={isOpen}>
+      <S.Overlay aria-hidden={isOpen} />
+      <S.IconWrapper>
+        <FilterList
+          size={18}
+          aria-label="open filters"
+          onClick={() => setIsOpen(true)}
+        />
+        <Close
+          size={18}
+          aria-label="close filters"
+          onClick={() => setIsOpen(false)}
+        />
+      </S.IconWrapper>
 
-          {item.type === 'checkbox'
-            ? item.fields.map((field) => (
-                <Checkbox
-                  key={field.name}
-                  name={field.name}
-                  labelFor={field.name}
-                  label={field.label}
-                  isChecked={!!values[field.name]}
-                />
-              ))
-            : item.fields.map((field) => (
-                <Radio
-                  key={field.name}
-                  id={field.name}
-                  labelFor={field.name}
-                  name={item.name}
-                  label={field.label}
-                  value={field.name}
-                  defaultChecked={field.name === values[item.name]}
-                />
-              ))}
-        </div>
-      ))}
+      <S.Content>
+        {items.map((item) => (
+          <S.Items key={item.title}>
+            <Heading lineBottom lineColor="secondary" size="small">
+              {item.title}
+            </Heading>
 
-      <Button fullWidth>Filter</Button>
+            {item.type === 'checkbox'
+              ? item.fields.map((field) => (
+                  <Checkbox
+                    key={field.name}
+                    name={field.name}
+                    labelFor={field.name}
+                    label={field.label}
+                    isChecked={!!values[field.name]}
+                    onCheck={(v) => handleChange(field.name, v)}
+                  />
+                ))
+              : item.fields.map((field) => (
+                  <Radio
+                    key={field.name}
+                    id={field.name}
+                    labelFor={field.name}
+                    name={item.name}
+                    label={field.label}
+                    value={field.name}
+                    defaultChecked={field.name === values[item.name]}
+                    onChange={() => handleChange(item.name, field.name)}
+                  />
+                ))}
+          </S.Items>
+        ))}
+      </S.Content>
+
+      <S.Footer>
+        <Button fullWidth onClick={handleFilter}>
+          Filter
+        </Button>
+      </S.Footer>
     </S.Wrapper>
   )
 }
--- src/components/ExploreSidebar/test.tsx
+++ src/components/ExploreSidebar/test.tsx
@@ -1,12 +1,17 @@
-import { screen } from '@testing-library/react'
+import { screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { css } from 'styled-components'
 import { renderWithTheme } from 'utils/tests/helpers'
 import ExploreSidebar from '.'
 
 import mockSidebarItems from './mock'
+import { Overlay } from './styles'
 
 describe('<ExploreSidebar />', () => {
   it('should render headings', () => {
-    renderWithTheme(<ExploreSidebar items={mockSidebarItems} />)
+    renderWithTheme(
+      <ExploreSidebar items={mockSidebarItems} onFilter={jest.fn} />
+    )
 
     expect(screen.getByRole('heading', { name: /price/i })).toBeInTheDocument()
 
@@ -20,7 +25,9 @@ describe('<ExploreSidebar />', () => {
   })
 
   it('should render inputs', () => {
-    renderWithTheme(<ExploreSidebar items={mockSidebarItems} />)
+    renderWithTheme(
+      <ExploreSidebar items={mockSidebarItems} onFilter={jest.fn} />
+    )
 
     expect(
       screen.getByRole('checkbox', { name: /under \$50/i })
@@ -32,7 +39,9 @@ describe('<ExploreSidebar />', () => {
   })
 
   it('should render filter button', () => {
-    renderWithTheme(<ExploreSidebar items={mockSidebarItems} />)
+    renderWithTheme(
+      <ExploreSidebar items={mockSidebarItems} onFilter={jest.fn} />
+    )
 
     expect(screen.getByRole('button', { name: /filter/i })).toBeInTheDocument()
   })
@@ -42,10 +51,100 @@ describe('<ExploreSidebar />', () => {
       <ExploreSidebar
         items={mockSidebarItems}
         initialValues={{ windows: true, sort_by: 'low-to-high' }}
+        onFilter={jest.fn}
       />
     )
 
     expect(screen.getByRole('checkbox', { name: /windows/i })).toBeChecked()
     expect(screen.getByRole('radio', { name: /low to high/i })).toBeChecked()
+  })
+
+  it('should filter with initial values', async () => {
+    const onFilter = jest.fn()
+
+    renderWithTheme(
+      <ExploreSidebar
+        items={mockSidebarItems}
+        initialValues={{ windows: true, sort_by: 'low-to-high' }}
+        onFilter={onFilter}
+      />
+    )
+
+    const filterButton = screen.getByRole('button', { name: /filter/i })
+    userEvent.click(filterButton)
+
+    await waitFor(() => {
+      expect(onFilter).toBeCalledWith({ windows: true, sort_by: 'low-to-high' })
+    })
+  })
+
+  it('should filter with checked values', async () => {
+    const onFilter = jest.fn()
+
+    renderWithTheme(
+      <ExploreSidebar items={mockSidebarItems} onFilter={onFilter} />
+    )
+
+    userEvent.click(screen.getByLabelText(/windows/i))
+    userEvent.click(screen.getByLabelText(/linux/i))
+    userEvent.click(screen.getByLabelText(/low to high/i))
+
+    const filterButton = screen.getByRole('button', { name: /filter/i })
+    userEvent.click(filterButton)
+
+    await waitFor(() => {
+      expect(onFilter).toBeCalledWith({
+        windows: true,
+        linux: true,
+        sort_by: 'low-to-high'
+      })
+    })
+  })
+
+  it('should altern between radio options', async () => {
+    const onFilter = jest.fn()
+
+    renderWithTheme(
+      <ExploreSidebar items={mockSidebarItems} onFilter={onFilter} />
+    )
+
+    userEvent.click(screen.getByLabelText(/low to high/i))
+    userEvent.click(screen.getByLabelText(/high to low/i))
+
+    const filterButton = screen.getByRole('button', { name: /filter/i })
+    userEvent.click(filterButton)
+
+    await waitFor(() => {
+      expect(onFilter).toBeCalledWith({ sort_by: 'high-to-low' })
+    })
+  })
+
+  it('should open/close sidebar when filtering on mobile', async () => {
+    const { container } = renderWithTheme(
+      <ExploreSidebar items={mockSidebarItems} onFilter={jest.fn} />
+    )
+
+    const variant = {
+      media: '(max-width: 768px)',
+      modifier: String(css`
+        ${Overlay}
+      `)
+    }
+
+    const Element = container.firstChild
+
+    expect(Element).not.toHaveStyleRule('opacity', '1', variant)
+
+    userEvent.click(screen.getByLabelText(/open filters/i))
+
+    await waitFor(() => {
+      expect(Element).toHaveStyleRule('opacity', '1', variant)
+    })
+
+    userEvent.click(screen.getByLabelText(/close filters/i))
+
+    await waitFor(() => {
+      expect(Element).not.toHaveStyleRule('opacity', '1', variant)
+    })
   })
 })
