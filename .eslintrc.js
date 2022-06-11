4c831b9ea1d9e1d3e588f0431b3ca840f9dd3381
Thiago Pereira <thiagopereiramail@gmail.com>
2022-06-11 13:31:13 -0300

    Configurar a ordem dos imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,13 +25,47 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
-  plugins: ['react', '@typescript-eslint', 'react-hooks'],
+  plugins: ['react', '@typescript-eslint', 'react-hooks', 'simple-import-sort'],
   rules: {
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
     'react/prop-types': 'off',
     'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-non-null-assertion': 'off'
-  }
+    '@typescript-eslint/no-non-null-assertion': 'off',
+
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error'
+  },
+  overrides: [
+    // override "simple-import-sort" config
+    {
+      files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
+      rules: {
+        'simple-import-sort/imports': [
+          'error',
+          {
+            groups: [
+              // Packages `next` related packages come first.
+              ['^(next|@next)'],
+              // Packages `react` related packages come seconds.
+              ['^(react|@react)'],
+              // Packages `@` related packages come seconds.
+              ['^@?\\w'],
+              // Template packages.
+              ['^(template)(/.*|$)'],
+              // Component packages.
+              ['^(components)(/.*|$)'],
+              // Parent imports. Put `..` last.
+              ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+              // Other relative imports. Put same-folder imports and `.` last.
+              ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+              // Style imports.
+              ['^(style)(/.*|$)']
+            ]
+          }
+        ]
+      }
+    }
+  ]
 }
